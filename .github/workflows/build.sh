591f0ebb09b17e266754be53d66c7934f0f02a1d
thebaptiste <jean-baptiste.veslin@meteo.fr>
2020-11-05 10:05:04 +0100

    build: add make doc, test and rpm

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -20,3 +20,13 @@ if test "${OUTPUT}" != ""; then
     git diff
     exit 1
 fi
+MAKE_DOC="/pub/metwork/continuous_integration/buildlogs/${B}/${R}/${OS_VERSION}/${GITHUB_RUN_NUMBER}/make_doc.log"
+if test -d docs; then make docs >"${MAKE_DOC}" 2>&1 || ( tail -200 "${MAKE_DOC}" ; exit 1 ); fi
+if test -d doc; then make doc >"${MAKE_DOC}" 2>&1 || ( tail -200 "${MAKE_DOC}" ; exit 1 ); fi
+rm -Rf html_doc
+if test -d /opt/metwork-${MFMODULE_LOWERCASE}-${TARGET_DIR}/html_doc; then cp -Rf /opt/metwork-${MFMODULE_LOWERCASE}-${TARGET_DIR}/html_doc . ; fi
+MAKE_TEST="/pub/metwork/continuous_integration/buildlogs/${B}/${R}/${OS_VERSION}/${GITHUB_RUN_NUMBER}/make_test.log"
+make test >"${MAKE_TEST}" 2>&1 || ( tail -200 "${MAKE_TEST}" ; exit 1 )
+MAKE_RPM="/pub/metwork/continuous_integration/buildlogs/${B}/${R}/${OS_VERSION}/${GITHUB_RUN_NUMBER}/make_rpm.log"
+make RELEASE_BUILD=${GITHUB_RUN_NUMBER} rpm >"${MAKE_RPM}" 2>&1 || ( tail -200 "${MAKE_RPM}" ; exit 1 )
+mv /opt/metwork-${MFMODULE_LOWERCASE}-${TARGET_DIR}/*.rpm .
