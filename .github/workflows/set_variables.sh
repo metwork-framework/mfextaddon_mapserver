4eff8d943743fbdcd47201aaf1e5c45561f81e6c
thebaptiste <jean-baptiste.veslin@meteo.fr>
2020-11-09 12:35:35 +0100

    build: add TARGET_DIR

--- .github/workflows/set_variables.sh
+++ .github/workflows/set_variables.sh
@@ -18,12 +18,15 @@ R=${GITHUB_REPOSITORY#metwork-framework/}
 B=${GITHUB_REF#refs/heads/}
 case "${GITHUB_REF}" in
     refs/heads/experimental* | refs/heads/master | refs/heads/release_*)
-        REF_BRANCH=${B};;
+        REF_BRANCH=${B}
+        TARGET_DIR=${B##release_};;
     *)
-        REF_BRANCH=integration;;
+        REF_BRANCH=integration
+        TARGET_DIR=master;;
 esac
 
-echo "::set-output name=refbranch::${REF_BRANCH}"
+echo "::set-output name=ref_branch::${REF_BRANCH}"
+echo "::set-output name=target_dir::${TARGET_DIR}"
 echo "::set-output name=buildimage::metwork/${MFBUILD}-${OS_VERSION}-buildimage:${REF_BRANCH}"
 echo "::set-output name=testimage::metwork/${MFBUILD}-${OS_VERSION}-testimage:${REF_BRANCH}"
 echo "::set-output name=buildlog_dir::/pub/metwork/continuous_integration/buildlogs/${B}/${R}/${OS_VERSION}/${GITHUB_RUN_NUMBER}"
