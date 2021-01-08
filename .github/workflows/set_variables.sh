2019cfb5f26c0d9b291abd8f5817f37243c93aef
thebaptiste <jean-baptiste.veslin@meteo.fr>
2021-01-08 12:24:11 +0100

    feat: publish doc

--- .github/workflows/set_variables.sh
+++ .github/workflows/set_variables.sh
@@ -31,3 +31,4 @@ echo "::set-output name=buildimage::metwork/${MFBUILD}-${OS_VERSION}-buildimage:
 echo "::set-output name=testimage::metwork/${MFBUILD}-${OS_VERSION}-testimage:${REF_BRANCH}"
 echo "::set-output name=buildlog_dir::/pub/metwork/continuous_integration/buildlogs/${B}/${R}/${OS_VERSION}/${GITHUB_RUN_NUMBER}"
 echo "::set-output name=rpm_dir::/pub/metwork/continuous_integration/rpms/${B}/${OS_VERSION}"
+echo "::set-output name=doc_dir::/pub/metwork/continuous_integration/docs/${B}/${R}"
