d04552f90c93a9a95e1903467dac94cdb14e08d5
thebaptiste <jean-baptiste.veslin@meteo.fr>
2021-01-11 14:15:01 +0100

    Revert "feat: cache buildimage"
    This reverts commit a5d1cf2925fa4e2676660c5f785a0f93fcdef4d2.

--- .github/workflows/set_variables.sh
+++ .github/workflows/set_variables.sh
@@ -32,7 +32,7 @@ esac
 echo "::set-output name=branch::${B}"
 echo "::set-output name=ref_branch::${REF_BRANCH}"
 echo "::set-output name=target_dir::${TARGET_DIR}"
-echo "::set-output name=buildimage::metwork/${MFBUILD}-${OS_VERSION}-buildimage"
+echo "::set-output name=buildimage::metwork/${MFBUILD}-${OS_VERSION}-buildimage:${REF_BRANCH}"
 echo "::set-output name=testimage::metwork/${MFBUILD}-${OS_VERSION}-testimage:${REF_BRANCH}"
 echo "::set-output name=buildlog_dir::/pub/metwork/continuous_integration/buildlogs/${B}/${R}/${OS_VERSION}/${GITHUB_RUN_NUMBER}"
 echo "::set-output name=rpm_dir::/pub/metwork/continuous_integration/rpms/${B}/${OS_VERSION}"
