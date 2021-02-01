ef3eaf1a8316aeb8bda8d7a5ca85fd93191407b9
metworkbot <metworkbot@metwork-framework.org>
2021-02-01 11:44:21 +0000

    build: sync common files from github_organization_management repository

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -24,7 +24,7 @@ cd /src
 
 
 
-mkdir -p "/opt/metwork-mfext}-${TARGET_DIR}"
+mkdir -p "/opt/metwork-mfext-${TARGET_DIR}"
 
 mkdir -p buildlogs
 export BUILDLOGS=buildlogs
@@ -44,7 +44,7 @@ fi
 #if test -f /opt/metwork-mfext-${TARGET_DIR}/.dhash; then cat /opt/metwork-mfext-${TARGET_DIR}/.dhash; fi
 #cat module_hash.debug |sort |uniq ; rm -f module_hash.debug
 #echo "${MODULEHASH}${DRONE_TAG}${DRONE_BRANCH}" |md5sum |cut -d ' ' -f1 >.build_hash
-#if test -f "${BUILDCACHE}/build_hash_mfext}_${BRANCH}_`cat .build_hash`"; then
+#if test -f "${BUILDCACHE}/build_hash_mfext_${BRANCH}_`cat .build_hash`"; then
 #    echo "::set-output name=bypass::true"
 #    exit 0
 #fi
@@ -52,15 +52,15 @@ fi
 if test -d docs; then make docs >${BUILDLOGS}/make_doc.log 2>&1 || ( tail -200 ${BUILDLOGS}/make_doc.log ; exit 1 ); fi
 if test -d doc; then make doc >${BUILDLOGS}/make_doc.log 2>&1 || ( tail -200 ${BUILDLOGS}/make_doc.log ; exit 1 ); fi
 rm -Rf html_doc
-if test -d /opt/metwork-mfext}-${TARGET_DIR}/html_doc; then cp -Rf /opt/metwork-mfext}-${TARGET_DIR}/html_doc . ; fi
+if test -d /opt/metwork-mfext-${TARGET_DIR}/html_doc; then cp -Rf /opt/metwork-mfext-${TARGET_DIR}/html_doc . ; fi
 make test >${BUILDLOGS}/make_test.log 2>&1 || ( tail -200 ${BUILDLOGS}/make_test.log ; exit 1 )
 make RELEASE_BUILD=${GITHUB_RUN_NUMBER} rpm >${BUILDLOGS}/make_rpm.log 2>&1 || ( tail -200 ${BUILDLOGS}/make_rpm.log ; exit 1 )
 
 mkdir rpms
-mv /opt/metwork-mfext}-${TARGET_DIR}/*.rpm rpms
+mv /opt/metwork-mfext-${TARGET_DIR}/*.rpm rpms
 
-#rm -f ${BUILDCACHE}/build_hash_mfext}_${BRANCH}_*
-#touch ${BUILDCACHE}/build_hash_mfext}_${BRANCH}_`cat .build_hash`
+#rm -f ${BUILDCACHE}/build_hash_mfext_${BRANCH}_*
+#touch ${BUILDCACHE}/build_hash_mfext_${BRANCH}_`cat .build_hash`
 #ls -l ${BUILDCACHE}
 
 #echo "::set-output name=bypass::false"
--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 
 
 
-[![Drone CI](http://metwork-framework.org:8000/api/badges/metwork-framework/mfextaddon_mapserver/status.svg)](http://metwork-framework.org:8000/metwork-framework/mfextaddon_mapserver)
+[![GitHub CI](https://github.com/metwork-framework/mfextaddon_mapserver/workflows/CI/badge.svg?branch=master)](https://github.com/metwork-framework/mfextaddon_mapserver/actions?query=workflow%3ACI&branch=master)
 [![Maintenance](https://github.com/metwork-framework/resources/blob/master/badges/maintained.svg)]()
 [![Gitter](https://github.com/metwork-framework/resources/blob/master/badges/community-en.svg)](https://gitter.im/metwork-framework/community-en?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge)
 [![Gitter](https://github.com/metwork-framework/resources/blob/master/badges/community-fr.svg)](https://gitter.im/metwork-framework/community-fr?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge)
