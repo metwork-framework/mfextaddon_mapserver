0866d8e84d338cbd8e4789ad7965eca597b9070b
Jean-Baptiste VESLIN <jean-baptiste.veslin@meteo.fr>
2022-06-17 12:37:55 +0200

    feat: upgrade mapserver from 7.4.2 to 7.6.4 (required with proj8) (#66)

--- layers/layer3_mapserver/0500_mapserver/Makefile.mk
+++ layers/layer3_mapserver/0500_mapserver/Makefile.mk
@@ -2,10 +2,10 @@ include ../../../adm/root.mk
 include $(MFEXT_HOME)/share/package.mk
 
 export NAME=mapserver
-export VERSION=7.4.2
+export VERSION=7.6.4
 export EXTENSION=tar.gz
 export CHECKTYPE=MD5
-export CHECKSUM=500c44fa1954af0c6a201ff4cae5235b
+export CHECKSUM=892c3ec0668124bde1078e6ef516750b
 DESCRIPTION=\
 MapServer is an Open Source platform for publishing spatial data and interactive mapping applications to the web.
 WEBSITE=http://mapserver.org
