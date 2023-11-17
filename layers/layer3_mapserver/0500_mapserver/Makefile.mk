1c1928b3d0c4ff409adbbeda5c785ee952c5086e
thebaptiste <jean-baptiste.veslin@meteo.fr>
2023-11-17 10:01:22 +0000

    feat: upgrade to mapserver 8 and add mappyfile

--- layers/layer3_mapserver/0500_mapserver/Makefile.mk
+++ layers/layer3_mapserver/0500_mapserver/Makefile.mk
@@ -2,10 +2,10 @@ include ../../../adm/root.mk
 include $(MFEXT_HOME)/share/package.mk
 
 export NAME=mapserver
-export VERSION=7.6.5
+export VERSION=8.0.1
 export EXTENSION=tar.gz
 export CHECKTYPE=MD5
-export CHECKSUM=8b19c5ba98ce567a5eae8eb09068f71f
+export CHECKSUM=c85b9b734471da431958b6331aae0803
 DESCRIPTION=\
 MapServer is an Open Source platform for publishing spatial data and interactive mapping applications to the web.
 WEBSITE=http://mapserver.org
@@ -13,4 +13,5 @@ LICENSE=MIT
 
 all:: $(PREFIX)/bin/mapserv
 $(PREFIX)/bin/mapserv:
-	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard OPTIONS="-DWITH_CURL=1 -DWITH_GIF=0 -DWITH_FCGI=0 -DWITH_PROTOBUFC=0 -DWITH_RSVG=1 -DCMAKE_PREFIX_PATH='$(PREFIX);$(PREFIX)/../scientific_core;$(PREFIX)/../core'" download uncompress configure_cmake cmake build_cmake install_cmake
+	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard OPTIONS="-DWITH_CURL=1 -DWITH_GIF=0 -DWITH_FCGI=0 -DWITH_PROTOBUFC=1 -DWITH_RSVG=1 -DCMAKE_PREFIX_PATH='$(PREFIX);$(PREFIX)/../scientific_core;$(PREFIX)/../core'" download uncompress configure_cmake cmake build_cmake install_cmake
+	mv $(PREFIX)/etc/mapserver-sample.conf $(PREFIX)/etc/mapserver.conf
