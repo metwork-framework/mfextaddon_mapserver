97ba8cd8c86714960ab388575f41580f16823d5f
thebaptiste <jean-baptiste.veslin@meteo.fr>
2023-11-17 10:46:04 +0000

    build: uggly fix

--- layers/layer3_mapserver/0500_mapserver/Makefile.mk
+++ layers/layer3_mapserver/0500_mapserver/Makefile.mk
@@ -14,4 +14,4 @@ LICENSE=MIT
 all:: $(PREFIX)/bin/mapserv
 $(PREFIX)/bin/mapserv:
 	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard OPTIONS="-DWITH_CURL=1 -DWITH_GIF=0 -DWITH_FCGI=0 -DWITH_PROTOBUFC=1 -DWITH_RSVG=1 -DCMAKE_PREFIX_PATH='$(PREFIX);$(PREFIX)/../scientific_core;$(PREFIX)/../core'" download uncompress configure_cmake cmake build_cmake install_cmake
-	mv $(PREFIX)/etc/mapserver-sample.conf $(PREFIX)/etc/mapserver.conf
+	mv $(PREFIX)/etc/mapserver-sample.conf $(PREFIX)/etc/mapserver.conf || mv /etc/$(PREFIX)/mapserver-sample.conf $(PREFIX)/etc/mapserver.conf
