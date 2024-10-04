3c44c69cd0e5a3abe437af695bafba2a57598bc9
Jean-Baptiste VESLIN <19856429+thebaptiste@users.noreply.github.com>
2024-10-04 17:29:19 +0200

    feat: upgrade mapserver from 7.6.5 to 8.2.2 and add mappyfile (#83)

--- .metwork-framework/components.md
+++ .metwork-framework/components.md
@@ -1,7 +1,10 @@
 | Name | Version | Layer |
 | --- | --- | --- |
-| [mapserver](http://mapserver.org) | 7.6.5 | mapserver |
+| [jsonref](https://pypi.org/project/jsonref) | 1.1.0 | python3_mapserverapi |
+| [lark](https://github.com/lark-parser/lark) | 1.1.8 | python3_mapserverapi |
+| [mappyfile](http://github.com/geographika/mappyfile) | 1.0.0 | python3_mapserverapi |
+| [mapserver](http://mapserver.org) | 8.2.2 | mapserver |
 | [mapserverapi](https://github.com/metwork-framework/mapserverapi) | 0.1.6 | mapserver |
 | [mapserverapi](https://github.com/metwork-framework/mapserverapi_python) | c90f709 | python3_mapserverapi |
 
-*(3 components)*
+*(6 components)*
--- layers/layer3_mapserver/0500_mapserver/Makefile.mk
+++ layers/layer3_mapserver/0500_mapserver/Makefile.mk
@@ -2,15 +2,21 @@ include ../../../adm/root.mk
 include $(MFEXT_HOME)/share/package.mk
 
 export NAME=mapserver
-export VERSION=7.6.5
+export VERSION=8.2.2
 export EXTENSION=tar.gz
 export CHECKTYPE=MD5
-export CHECKSUM=8b19c5ba98ce567a5eae8eb09068f71f
+export CHECKSUM=9bc8e375493f59ac1ef282b127096ec4
 DESCRIPTION=\
 MapServer is an Open Source platform for publishing spatial data and interactive mapping applications to the web.
 WEBSITE=http://mapserver.org
 LICENSE=MIT
 
-all:: $(PREFIX)/bin/mapserv
+all:: $(PREFIX)/bin/mapserv $(PREFIX)/etc/mapserver.conf
 $(PREFIX)/bin/mapserv:
-	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard OPTIONS="-DWITH_CURL=1 -DWITH_GIF=0 -DWITH_FCGI=0 -DWITH_PROTOBUFC=0 -DWITH_RSVG=1 -DCMAKE_PREFIX_PATH='$(PREFIX);$(PREFIX)/../scientific_core;$(PREFIX)/../core'" download uncompress configure_cmake cmake build_cmake install_cmake
+	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard OPTIONS="-DWITH_CURL=1 -DWITH_GIF=0 -DWITH_FCGI=0 -DWITH_PROTOBUFC=1 -DWITH_RSVG=1 -DCMAKE_PREFIX_PATH='$(PREFIX);$(PREFIX)/../scientific_core;$(PREFIX)/../core'" download uncompress configure_cmake cmake build_cmake install_cmake
+	mkdir -p $(PREFIX)/etc $(PREFIX)/tests $(PREFIX)/share/ogcapi
+	cp build/$(NAME)-$(VERSION)/tests/test.map $(PREFIX)/tests
+	cp -R build/$(NAME)-$(VERSION)/share/ogcapi/templates $(PREFIX)/share/mapserver/ogcapi
+
+$(PREFIX)/etc/mapserver.conf:
+	cat mapserver.conf | sed "s:PREFIX:$(PREFIX):" > $(PREFIX)/etc/mapserver.conf
